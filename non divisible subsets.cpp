0a2f416c391f297fc5aa705582dacad8eab89c35
Tummala Pallavi Reddy <109154814+pallavireddytummala@users.noreply.github.com>
2023-08-21 22:23:03 +0530

    Create non divisible subsets.cpp

---
+++ non divisible subsets.cpp
@@ -0,0 +1,43 @@
+//Given an array of N unique numbers, find the maximum length subset that can be formed such that sum of any 2 numbers in the subset is not a multiple of K.
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        vector<int>cnt(k,0);
+        for(int i=0;i<n;i++){
+            int x=(arr[i]%k);
+            cnt[x]++;
+        }
+        int count=0;
+        for(int i=1;i<k;i++){
+            if((i+i)%k==0 && cnt[i]>0){
+                count+=1;
+            }
+            else{
+                int j=max(cnt[i],cnt[k-i]);
+                count=count+j;
+            }
+        }
+        if(count%2==0)
+        count=count/2;
+        else
+            count=(count/2)+1;
+        if(cnt[0]>0){
+            count++;
+        }
+        cout<<count<<endl;
+    }
+    return 0;
+}
